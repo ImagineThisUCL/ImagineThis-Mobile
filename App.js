07476224d54ff7c533118ea520d55edb5b85d04e
Vojta Adam <bluetrox@hotmail.com>
2020-07-23 19:35:25 +0200

    Added styling and icons to bottom tab nav, cleaned imports in App.js

--- App.js
+++ App.js
@@ -1,9 +1,6 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { StyleSheet, Text, View} from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-import base from "./assets/baseStyle"
 import SetUpView from "./components/views/SetUp"
 import SetUpInlineView from "./components/views/SetUpInline"
 import SetUpComponentsView from "./components/views/SetUpComponents"
@@ -13,19 +10,66 @@ import ContactView from "./components/views/Home"
 import SettingsView from "./components/views/Home"
 import WellbeingView from "./components/views/Home"
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Ionicons from "@expo/vector-icons/Ionicons"
 
 const Tab = createBottomTabNavigator();
 
 function HomeScreen() {
-  return (
-    <Tab.Navigator>
-        <Tab.Screen name="Network" component={NetworkView} />
-        <Tab.Screen name="Well-being" component={WellbeingView} />
-        <Tab.Screen name="Home" component={HomeView} />
-        <Tab.Screen name="Contact" component={ContactView} />
-        <Tab.Screen name="Settings" component={SettingsView} />
-    </Tab.Navigator>
-  );
+    return (
+        <Tab.Navigator
+            tabBarOptions={{
+                activeTintColor: "#147EFB",
+                inactiveTintColor: "#000000",
+                labelStyle: {
+                    fontSize: 12,
+                },
+                style: {
+                    backgroundColor: "#D5E6EC",
+                    paddingBottom: 2
+                },
+            }}>
+            <Tab.Screen 
+                name="Network" 
+                component={NetworkView}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="ios-contacts" color={color} size={size} />
+                    )
+                }}/>
+            <Tab.Screen 
+                name="Well-being"
+                component={WellbeingView}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="ios-heart" color={color} size={size} />
+                    )
+                }}/>
+            <Tab.Screen 
+                name="Home"
+                component={HomeView} 
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="ios-home" color={color} size={size} />
+                    )
+                }}/>
+            <Tab.Screen 
+                name="Contact"
+                component={ContactView}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="ios-mail" color={color} size={size} />
+                    )
+                }}/>
+            <Tab.Screen 
+                name="Settings"
+                component={SettingsView}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="ios-settings" color={color} size={size} />
+                    )
+                }}/>
+        </Tab.Navigator>
+    );
 }
 
 const Stack = createStackNavigator();
