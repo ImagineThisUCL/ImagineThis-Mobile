86c6d61b41ef14d21fa09d377c93e36f36980e46
Vojta Adam <bluetrox@hotmail.com>
2020-07-23 19:51:21 +0200

    Copied figma design to SetUpComponents

--- components/views/SetUpComponents.js
+++ components/views/SetUpComponents.js
@@ -14,23 +14,18 @@ class SetUpComponents extends Component {
                 </P>
                 <P style={{color: "#ffffff", fontSize: 21}}>
                     A carer support worker will have recommended this application to you.  They recognise that carers often forget to look after themselves.  This app has been designed to encourage you to do this.{"\n\n"}
-                    Occasionally it will nudge you to keep in contact with people you like to speak to, or persue activities with them.  It will also make you aware of new local groups with similar interests to yourself.
-                </P>
-                <P style={{color: "#ffffff", fontSize: 21}}>
-                    Occasionally it will nudge you to keep in contact with people you like to speak to, or persue activities with them.  It will also make you aware of new local groups with similar interests to yourself.
-                </P>
-                <P style={{color: "#ffffff", fontSize: 21}}>
+                    Occasionally it will nudge you to keep in contact with people you like to speak to, or persue activities with them.  It will also make you aware of new local groups with similar interests to yourself.{"\n\n"}
                     If you consent to this click Save.
                 </P>
                 <View style={{flexDirection: "row", justifyContent: "space-between"}}>
                     <Button
-                        style={{backgroundColor: "#5555ff", borderRadius: 25, marginTop: base.margin}}
-                        textStyle={{color: "#ffffff"}}>
+                        style={{backgroundColor: "#ffffff", borderRadius: 25, marginTop: base.margin}}
+                        textStyle={{color: "#147EFB", fontWeight: "bold"}}>
                         No Thanks
                     </Button>
                     <Button
-                        style={{backgroundColor: "#5555ff", borderRadius: 25, marginTop: base.margin}}
-                        textStyle={{color: "#ffffff"}}>
+                        style={{backgroundColor: "#ffffff", borderRadius: 25, marginTop: base.margin}}
+                        textStyle={{color: "#147EFB", fontWeight: "bold"}}>
                         Save
                     </Button>
                 </View>
