aca9708f79c878c2b371d99b61922c504c55492e
Vojta Adam <bluetrox@hotmail.com>
2020-07-23 15:44:10 +0200

    Changing style properties for P

--- components/views/SetUpComponents.js
+++ components/views/SetUpComponents.js
@@ -9,16 +9,17 @@ class SetUpComponents extends Component {
     render() {
         return (
             <View style={{flex: 1, padding: 10, backgroundColor: "#11287B"}}>
-                <P white largeFont>
+                <P style={{color: "#ffffff", fontSize: 21}}>
                     CarerCare V0120-21: Set Up
                 </P>
-                <P white largeFont>
-                    A carer support worker will have recommended this application to you.  They recognise that carers often forget to look after themselves.  This app has been designed to encourage you to do this.
-                </P>
-                <P white largeFont>
+                <P style={{color: "#ffffff", fontSize: 21}}>
+                    A carer support worker will have recommended this application to you.  They recognise that carers often forget to look after themselves.  This app has been designed to encourage you to do this.{"\n\n"}
                     Occasionally it will nudge you to keep in contact with people you like to speak to, or persue activities with them.  It will also make you aware of new local groups with similar interests to yourself.
                 </P>
-                <P white largeFont>
+                <P style={{color: "#ffffff", fontSize: 21}}>
+                    Occasionally it will nudge you to keep in contact with people you like to speak to, or persue activities with them.  It will also make you aware of new local groups with similar interests to yourself.
+                </P>
+                <P style={{color: "#ffffff", fontSize: 21}}>
                     If you consent to this click Save.
                 </P>
                 <View style={{flexDirection: "row", justifyContent: "space-between"}}>
