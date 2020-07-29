981b840ae1d14c1290d7e5a56e9ba616931554a1
Jeremy <scjc885@gmail.com>
2020-07-29 05:26:07 +0100

    added Iconbutton reuseable

---
+++ components/reusables/IconButton.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react"
+import { StyleSheet } from "react-native"
+import { Button } from 'react-native-elements';
+
+import base from "../../assets/baseStyle"
+
+const styles = StyleSheet.create({
+    button: {
+        flex: 1
+    },
+})
+
+export default class IconButton extends Component {
+    render() {
+        let buttonStyleList = [styles.button]
+        let textStyleList = [styles.text]
+
+        return (
+            <Button
+                {...this.props}
+                style={[...buttonStyleList, this.props.style]}
+                type="clear"
+            />
+            // <TouchableOpacity
+            //     {...this.props}
+            //     style={[...buttonStyleList, this.props.style]}>
+            //     // <Image
+            //     //     source={require(this.props.iconName)}
+            //     //     style={styles.ImageIconStyle}
+            //     // />
+            //     <Ionicons {...this.props} />
+            // </TouchableOpacity>
+        )
+    }
+}
