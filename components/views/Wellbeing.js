4fa6d0ea6129e43809b953d1a1032ae98fad7833
Vojtech Adam <bluetrox@hotmail.com>
2020-08-05 23:39:22 +0200

    Added chart templates into ReportForSharing (#4)

--- components/views/Wellbeing.js
+++ components/views/Wellbeing.js
@@ -33,6 +33,12 @@ export default class Wellbeing extends Component {
                     textStyle={{color: "#ffffff"}}>
                     Go to Weekly Confirm Wellbeing
                 </Button>
+                <Button
+                    onPress={() => this.props.navigation.navigate('ReportForSharing')}
+                    style={{backgroundColor: "#5555ff", borderRadius: 25, marginTop: base.margin}}
+                    textStyle={{color: "#ffffff"}}>
+                    Go to Report for Sharing
+                </Button>
             </View>
         )
     }
