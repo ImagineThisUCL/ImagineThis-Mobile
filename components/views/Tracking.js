a49f84c88239d901b5cd647404585ac4d00e6e94
Jeremy <scjc885@gmail.com>
2020-08-07 05:01:19 +0100

    added toggle switch to tracking.js

--- components/views/Tracking.js
+++ components/views/Tracking.js
@@ -1,22 +1,19 @@
 // Recreating: Care Network Page
 
-import { View, ScrollView, Text, TextInput, Switch} from "react-native"
+import { View, ScrollView, useState} from "react-native"
 import React, { Component } from "react"
 import P from "../reusables/P"
 import Button from "../reusables/Button"
 import IconButton from "../reusables/IconButton"
-import InputField from "../reusables/InputField"
 import { Card } from 'react-native-elements';
 import { Feather } from '@expo/vector-icons';
+import Toggle from "../reusables/Toggle"
 
 import base from "../../assets/baseStyle"
 
 class Tracking extends Component {
 
-
     render() {
-        const [isEnabled, setIsEnabled] = useState(false);
-        const toggleSwitch = () => setIsEnabled(previousState => !previousState);
 
         return (
             <ScrollView style={{flex: 1, padding: 10, backgroundColor: "#11287B"}}>
@@ -44,15 +41,13 @@ class Tracking extends Component {
                         <P style={{fontSize: 18, textAlign: 'center', flex: 2, color: 'white'}}>
                              Click the OK button below to consent to the creation of a map that enables you and other app users to understand what support activties help people in your area.  By consenting you will not be sharing personally identifiable data.  All data used to create the map will be annonymised to protect your privacy.
                         </P>
-                        <View style={{flex: 1}}>
-                            <Switch
-                                   trackColor={{ false: "#767577", true: "#81b0ff" }}
-                                   thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
-                                   ios_backgroundColor="#3e3e3e"
-                                   onValueChange={toggleSwitch}
-                                   value={isEnabled}
-                                 />
-                        </View>
+                        <Toggle
+                                trackColor={{ false: "#767577", true: "#81b0ff" }}
+                                thumbColor="white"
+                                ios_backgroundColor="#3e3e3e"
+                            //    onValueChange={toggleSwitch}
+                            //    value={isEnabled}
+                        />
                     </View>
                 </Card>
 
