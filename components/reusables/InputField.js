18814a9c21681ed8f66db20317c2e7c4a566ffb0
Jeremy <ucabsjc@ucl.ac.uk>
2020-08-13 05:07:06 +0100

    added prop types for input field reusable

--- components/reusables/InputField.js
+++ components/reusables/InputField.js
@@ -26,4 +26,21 @@ export default class InputField extends Component {
 }
 
 InputField.propTypes = {
+    // props for core TextInput 
+    placeholder: propTypes.string,
+
+    // props for input from react-native-elements
+    containerStyle: propTypes.object,
+    disabled: propTypes.bool,
+    disabledInputStyle: propTypes.object,
+    inputContainerStyle: propTypes.object,
+    errorMessage: propTypes.string,
+    errorStyle: propTypes.object,
+    inputStyle: propTypes.object,
+    label: propTypes.string,
+    labelStyle: propTypes.object,
+    leftIcon: propTypes.object, // not sure
+    leftIconContainerStyle: propTypes.object,
+    rightIcon: propTypes.object, // not sure
+    rightIconContainerStyle: propTypes.object,
 }
