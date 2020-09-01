5ac91c37765412ef64f49ef3385746b0d8f96141
Vojta Adam <bluetrox@hotmail.com>
2020-09-01 23:42:01 +0200

    Fixed source code formatting in Alert box

--- components/reusables/AlertBox.js
+++ components/reusables/AlertBox.js
@@ -6,68 +6,59 @@ import {
     Text,
     View,
 } from "react-native"
-
-
 import Button from "../reusables/Button"
-
 import base from "../../assets/baseStyle"
 
 const styles = StyleSheet.create({
 
-    contentWrapper:{
-      flexDirection: "row",
-      justifyContent: "center",
-      alignItems: "center",
-      padding: 5,
-      marginVertical: 10,
-   },
-   bottomWrapper:{
-     flexDirection: "row",
-     justifyContent: "space-between",
-     padding: 5,
-   },
+    contentWrapper: {
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 5,
+        marginVertical: 10,
+    },
+    bottomWrapper: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        padding: 5,
+    },
 
-   contentTextStyle:{
-     fontSize: base.font.large,
-     textAlign: "center",
-   },
+    contentTextStyle: {
+        fontSize: base.font.large,
+        textAlign: "center",
+    },
+})
 
+export default class AlertBox extends Component {
+    render() {
 
- })
+        let contentTextStyle = [styles.contentTextStyle]
 
+        return (
 
-   export default class AlertBox extends Component {
-       render() {
-
-         let contentTextStyle= [styles.contentTextStyle]
-
-         return(
-
-           <View>
-             <View style={styles.contentWrapper}>
-               <Text
-                 style={[...contentTextStyle, this.props.textStyle]}>
-                 {this.props.children}
-               </Text>
-             </View>
-             <View style={styles.bottomWrapper}>
-                <Button
-                   onPress={() => alert("YES")}
-                   textStyle={{color:"#f11c30"}}>
-                    YES
+            <View>
+                <View style={styles.contentWrapper}>
+                    <Text
+                        style={[...contentTextStyle, this.props.textStyle]}>
+                        {this.props.children}
+                    </Text>
+                </View>
+                <View style={styles.bottomWrapper}>
+                    <Button
+                        onPress={() => alert("YES")}
+                        textStyle={{ color: "#f11c30" }}>
+                        YES
                 </Button>
-                <Button
-                   onPress={() => alert("NO")}
-                   textStyle={{color:"#579821"}}>
-                    NO
+                    <Button
+                        onPress={() => alert("NO")}
+                        textStyle={{ color: "#579821" }}>
+                        NO
                 </Button>
+                </View>
+            </View>
 
+        )
 
-        </View>
-        </View>
-
-
-         )
-
-   }
- }
+    }
+}
